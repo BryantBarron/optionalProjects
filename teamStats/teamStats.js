c9567d2342f56f890fe10d8558a5b90775d713a4
Bryant Barron <bwalt@Bryants-MacBook-Pro.local>
2019-01-08 13:21:11 -0800

    starting teamStat optionalProject

---
+++ teamStats/teamStats.js
@@ -0,0 +1,36 @@
+const team = {
+  _players: [
+    {
+      firstName: 'John',
+      lastName: 'Zoidberg',
+      age: 86
+    },
+    {
+      firstName: 'Turanga',
+      lastName: 'Leela',
+      age: 28
+    },
+    {
+      firstName: 'Philip',
+      lastName: 'Fry',
+      age: 1029
+    }
+  ],
+  _games: [
+    {
+      opponent: 'Maple Leafs',
+      teamPoints: 4,
+      opponentPoints: 0
+    },
+    {
+      opponent: 'Something, Something Darkside',
+      teamPoints: 3,
+      opponentPoints: 1
+    },
+    {
+      opponent: 'Snu, snu',
+      teamPoints: 1,
+      opponentPoints: 0
+    }
+  ],
+};
